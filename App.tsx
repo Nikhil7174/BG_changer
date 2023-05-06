e321ec5c2d7b7b02f8125742c36823bd75a0fe5a
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-05-06 16:43:19 +0530

    Added fincionality to generate custom colors

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
@@ -9,8 +10,17 @@ import React, {useState} from 'react';
 
 export default function App(): JSX.Element {
   const [randomBackground, setRandomBackground] = useState('#ffffff ');
+  const [customColor, setCustomColor] = useState('');
+  const [press, setPress] = useState(false);
+
+  const generateCustomColor = () => {
+    let color = customColor;
+    setCustomColor(color);
+    setPress(false);
+  };
 
   const generateColor = () => {
+    setPress(true);
     const hexRange = '0123456789ABCDEF';
     let color = '#';
 
@@ -22,11 +32,27 @@ export default function App(): JSX.Element {
 
   return (
     <>
-      <StatusBar backgroundColor={randomBackground} />
-      <View style={[styles.container, {backgroundColor: randomBackground}]}>
+      <StatusBar backgroundColor={press ? randomBackground : customColor} />
+      <View
+        style={[
+          styles.container,
+          {backgroundColor: press ? randomBackground : customColor},
+        ]}>
         <TouchableOpacity onPress={generateColor}>
           <View style={styles.actionBtn}>
-            <Text style={styles.actionBtntxt}>Press me</Text>
+            <Text style={styles.actionBtntxt}>Random Color</Text>
+          </View>
+        </TouchableOpacity>
+        <View style={styles.textInput}>
+          <TextInput
+            placeholder="Eg. #ffffff"
+            placeholderTextColor="gray"
+            onChangeText={newText => setCustomColor(newText)}
+          />
+        </View>
+        <TouchableOpacity onPress={generateCustomColor}>
+          <View style={styles.actionBtn2}>
+            <Text style={styles.actionBtntxt2}>Custom Color</Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -42,11 +68,30 @@ const styles = StyleSheet.create({
   },
   actionBtn: {
     borderRadius: 12,
-    backgroundColor: '#6A1B4D',
+    backgroundColor: '#013236',
     paddingVertical: 10,
     paddingHorizontal: 40,
   },
+  actionBtn2: {
+    marginTop: 10,
+    borderRadius: 12,
+    backgroundColor: '#110136',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  actionBtntxt2: {
+    fontSize: 14,
+    color: '#ffffff',
+  },
   actionBtntxt: {
     fontSize: 24,
+    color: '#ffffff',
+  },
+  textInput: {
+    backgroundColor: '#c5c5c5',
+    color: '#ffffff',
+    marginTop: 30,
+    borderRadius: 10,
+    paddingHorizontal: 40,
   },
 });
